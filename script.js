3967c0792c81647b60444f5e8e52350a4cc864dc
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-23 13:57:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-window.onload = function () {
+/* window.onload = function () {
   let c = document.getElementById("canvas1");
   let ctx = c.getContext("2d");
   let img = document.getElementById("img0");
   ctx.drawImage(img, 10, 10, 45, 45);
 };
+
+*/
+
+const btnElement = document.getElementById("button1");
+btnElement.addEventListener("click",function1);
+
+function function1() {
+  img1 = new Image()
+  img1.src = "symbFrameActvInstFdlyK.svg";
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
+  ctx.drawImage(img1, 10, 10, 45, 45);
+}
+
+
