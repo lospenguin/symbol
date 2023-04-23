7c00eb113d38a1fc1bc61693231583beb148e622
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-23 14:00:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+const btnElement = document.getElementById("button1");
+btnElement.addEventListener("click", function1);
+
+function function1() {
+  img1 = new Image();
+  img1.src = "./symbFrameActvInstFdlyK.svg";
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
+  ctx.drawImage(img1, 0, 0, 500, 500, 95, 45, 100, 100);
+}
+
+
 /* window.onload = function () {
   let c = document.getElementById("canvas1");
   let ctx = c.getContext("2d");
@@ -6,16 +19,3 @@
 };
 
 */
-
-const btnElement = document.getElementById("button1");
-btnElement.addEventListener("click",function1);
-
-function function1() {
-  img1 = new Image()
-  img1.src = "symbFrameActvInstFdlyK.svg";
-  let c = document.getElementById("canvas1");
-  let ctx = c.getContext("2d");
-  ctx.drawImage(img1, 10, 10, 45, 45);
-}
-
-
