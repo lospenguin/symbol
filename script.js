27dc39ee9b16e1f8cfd53d5eeb111ff79772cf48
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-23 14:04:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,3 @@ function function1() {
   ctx.drawImage(img1, 0, 0, 500, 500, 95, 45, 100, 100);
 }
 
-
-/* window.onload = function () {
-  let c = document.getElementById("canvas1");
-  let ctx = c.getContext("2d");
-  let img = document.getElementById("img0");
-  ctx.drawImage(img, 10, 10, 45, 45);
-};
-
-*/
