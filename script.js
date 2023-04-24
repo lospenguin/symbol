9fb494011fcb238a168c573a571b3e764c84a336
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-24 06:30:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 "use strict";
+let imgInput;
+let imgOutput;
+
+/*listen for button click on Create Button to trigger function1*/
+let btnClick = document.getElementById("button1");
+btnClick.addEventListener("click", function1);
+
+/*run through list of input selections in imgTileArea array to pull from selector and push to img output in html wrapper2*/
+function function1() {
+  for (let i = 0; i < imgTileArea.length; i++) {
+    imgInput = document.getElementById(imgTileArea[i][0]).value;
+    imgOutput = document.getElementById(imgTileArea[i][1]);
+    imgOutput.src = imgArrayName[imgInput];
+  }
+}
 
 /*array for storing all img file names*/
 const imgArrayName = [
@@ -13,17 +28,8 @@ const imgArrayName = [
   "symbAmpBCo.svg",
 ];
 
-/*listen for button click on Create Button to trigger function1*/
-let btnClick = document.getElementById("button1");
-btnClick.addEventListener("click", function1);
-
-function function1() {
-  let inputChpt2MainA = document.getElementById("inputChpt2MainA").value;
-  let outputChpt2MainA = document.getElementById("outputChpt2MainA");
-  outputChpt2MainA.src = imgArrayName[inputChpt2MainA];
-  let inputChpt2MainB = document.getElementById("inputChpt2MainB").value;
-  let outputChpt2MainB = document.getElementById("outputChpt2MainB");
-  outputChpt2MainB.src = imgArrayName[inputChpt2MainB];
-  console.log(inputChpt2MainA);
-  console.log(inputChpt2MainB);
-}
+/*array for storing all html wrapper modifier and amplifier user inputs paired with their output areas in html wrapper2 */
+const imgTileArea = [
+  ["inputChpt2MainA", "outputChpt2MainA"],
+  ["inputChpt2MainB", "outputChpt2MainB"],
+];
