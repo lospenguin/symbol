e842d8fdf226927531de1d0d62cda12c9e7810e8
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-23 21:29:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+"use strict";
 
-const btnElement = document.getElementById("button1");
-btnElement.addEventListener("click", function1);
+/*array for storing all img file names*/
+const imgArrayName = [
+  "blank.svg",
+  "symbFrameActvInstFdlyK.svg",
+  "symbFrameActvInstEnyK.svg",
+  "symbFrameActvInstNeutK.svg",
+  "symbFrameActvInstUnkK.svg",
+  "symbAmpBSqd.svg",
+  "symbAmpBSect.svg",
+  "symbAmpBPlt.svg",
+  "symbAmpBCo.svg",
+];
 
+/*listen for button click on Create Button to trigger function1*/
+let btnClick = document.getElementById("button1");
+btnClick.addEventListener("click", function1);
+
+/*reconfigure the source image locations on the screen to display the correct icon*/
 function function1() {
-  img1 = new Image();
-  img1.src = "./symbFrameActvInstFdlyK.svg";
-  let c = document.getElementById("canvas1");
-  let ctx = c.getContext("2d");
-  ctx.drawImage(img1, 0, 0, 500, 500, 95, 45, 100, 100);
+  let inputChpt2MainA = document.getElementById("inputChpt2MainA").value;
+  let outputChpt2MainA = document.getElementById("outputChpt2MainA");
+  outputChpt2MainA.src = imgArrayName[inputChpt2MainA];
 }
-
