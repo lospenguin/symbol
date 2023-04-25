d934c5bf83588c3c7afd47dfc2f5ce7eaf75d510
Los <122172220+lospenguin@users.noreply.github.com>
2023-04-25 07:22:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
-  ctx.lineWidth = 1.2;
-  ctx.reset();
-  ctx.beginPath();
-  ctx.moveTo(25,25);
-  ctx.lineTo(50,50);
-  ctx.stroke();
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = 1.2;
+ctx.reset();
+ctx.beginPath();
+ctx.moveTo(25,25);
+ctx.lineTo(50,50);
+ctx.stroke();
