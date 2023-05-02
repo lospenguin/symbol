b60ffe0d868e7f92ebcbfabefb8662563f0907ed
Los <122172220+lospenguin@users.noreply.github.com>
2023-05-01 21:04:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,167 +1,146 @@
 "use strict";
 
-/*listen for button click on Main Button to trigger function1*/
+let screenSel;
+let screenSelVal;
+let btnSelOpt1;
+let btnSelOpt2 = "empty";
+let btnSelOpt3;
+let btnSelOpt4;
+/*
+
+
+BREAK
+
+
+*/
+/*arrays for all main and mod icon selectors*/
+let arraySelectors = [
+  [
+    ["Select...", "img/blank.svg"],
+    ["Friendly", "img/mainAFrameFdly.svg"],
+    ["Enemy", "img/mainAFrameEny.svg"],
+    ["Neutral", "img/mainAFrameNeu.svg"],
+    ["Unknown", "img/mainAFrameUnk.svg"],
+  ],
+  [
+    ["Select...", "img/blank.svg"],
+    ["Team", "img/ampBTm.svg"],
+    ["Squad", "img/ampBSqd.svg"],
+    ["Section", "img/ampBSect.svg"],
+    ["Platoon", "img/ampBPlt.svg"],
+    ["Company", "img/ampBCo.svg"],
+    ["Battalion", "img/ampBBn.svg"],
+    ["Group/Rgmt", "img/ampBReg.svg"],
+    ["Brigade", "img/ampBBde.svg"],
+    ["Division", "img/ampBDiv.svg"],
+    ["Corps", "img/ampBCorps.svg"],
+    ["Theater Army", "img/ampBArmy.svg"],
+    ["Army Group", "img/ampBAGrp.svg"],
+    ["Theater", "img/ampBThtr.svg"],
+    ["Command", "img/ampBCmd.svg"],
+  ],
+  [
+    ["Select...", "img/blank.svg"],
+    ["Rifle", "img/blank.svg"],
+    ["Light . Machine Gun", "img/blank.svg"],
+    ["Medium . Machine Gun", "img/blank.svg"],
+    ["Heavy . Machine Gun", "img/blank.svg"],
+  ],
+];
+/*array for screenDisplay - primary array*/
+let arrayCategory = [
+  ["Select Category"],
+  ["Unit & Organizations", arraySelectors[0], arraySelectors[1]],
+  ["Equipment", arraySelectors[0], arraySelectors[2]],
+  ["Installation & Activities"],
+  ["Control Measures & Tasks"],
+];
+/*
+
+
+BREAK
+
+
+*/
+/* LISTENER BUTTONS . button click to go from main screen to symbol subsection */
 let touchEvent1 = "ontouchstart" in window ? "touchstart" : "click";
-document
-  .getElementById("btnMain")
-  .addEventListener(touchEvent1, funcMainScreen);
+document.getElementById("btnMain").addEventListener(touchEvent1, funcMain);
 
-/*listen for button click on Unit-Org Button to trigger function2*/
+/*button click to return from symbol subsection to main screen*/
 let touchEvent2 = "ontouchstart" in window ? "touchstart" : "click";
-document
-  .getElementById("btnReturn")
-  .addEventListener(touchEvent2, funcReturnMain);
+document.getElementById("btnReturn").addEventListener(touchEvent2, funcReturn);
 
-/*listen for button click on Unit-Org Button to trigger function2*/
+/*button click to generate symbol from symbol subsection*/
 let touchEvent3 = "ontouchstart" in window ? "touchstart" : "click";
-document.getElementById("button1").addEventListener(touchEvent3, funcUnitOrg);
+document.getElementById("btnCreate").addEventListener(touchEvent3, funcCreate);
+/*
 
+
+BREAK
+
+
+*/
 /*for return button, select values and the canvas are cleared*/
-function funcMainScreen() {
-  let screenSel = document.getElementById("typeMain");
-  let screenSelVal = screenSel.value;
-  if (screenSelVal === "1") {
+function funcMain() {
+  screenSel = document.getElementById("selMainCategory");
+  screenSelVal = screenSel.value;
+  if (screenSelVal !== "0") {
     document.getElementById("screenMain").style.display = "none";
-    document.getElementById("screenUnit").style.display = "block";
-    screenSelVal = undefined;
+    document.getElementById("screenDisplay").style.display = "block";
+    document.getElementById("selMainCategory").selectedIndex = 0;
+    document.getElementById("screenInfo").innerHTML =
+      arrayCategory[
+        screenSelVal
+      ][0]; /* get the first item, screenInfo element, from the list for the selected category */
+    for (let i = 0; i < arrayCategory[screenSelVal].length - 1; i++) {
+      /*create selector elements equal to the number of selectors for each category in arrayCategory */
+      let divSel = document.getElementById("divSel");
+      let sel = document.createElement("select");
+      sel.className = "selector";
+      sel.id = `selector${i}`;
+      btnSelOpt1 = i + 1;
+      divSel.appendChild(sel);
+      for (let i = 0; i < arrayCategory[screenSelVal][btnSelOpt1].length; i++) {
+        let opt = document.createElement("option");
+        opt.text = arrayCategory[screenSelVal][btnSelOpt1][i][0];
+        opt.value = i;
+        sel.appendChild(opt);
+      }
+    }
   }
 }
 
-function funcReturnMain() {
+function funcReturn() {
   document.getElementById("screenMain").style.display = "block";
-  document.getElementById("screenUnit").style.display = "none";
-  let canvas = document.getElementById("canvas1");
+  document.getElementById("screenDisplay").style.display = "none";
+  let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  let select = document.getElementById("inputChpt2MainA");
-  select.selectedIndex = 0;
-  let select2 = document.getElementById("inputChpt2ModB");
-  select2.selectedIndex = 0;
+  btnSelOpt1 = "0";
+  btnSelOpt2 = "0";
+  let removeSel = document.getElementById("divSel");
+  while (removeSel.firstChild) {
+    removeSel.removeChild(removeSel.firstChild);
+  }
 }
-
-/*unit org screen section to create unit org screen select items for main frame area A */
-/*array for storing all Main A unit icon frames*/
-const unitMainA = ["Select...", "Friendly", "Enemy", "Neutral", "Unknown"];
-/*append all unitMainA list items to Main A HTML selector */
-for (let i = 0; i < unitMainA.length; i++) {
-  let option = document.createElement("option");
-  option.text = unitMainA[i];
-  option.value = i;
-  let select = document.getElementById("inputChpt2MainA");
-  select.appendChild(option);
-}
-
-/*unit org screen section to create unit org echelon select items for mod B*/
-/*array for storing all Mod B unit echelon modifiers*/
-const unitModB = [
-  "Select...",
-  "Team",
-  "Squad",
-  "Section",
-  "Platoon",
-  "Company",
-  "Battalion",
-  "Group/Rgmt",
-  "Brigade",
-  "Division",
-  "Corps",
-  "Theater Army",
-  "Army Group",
-  "Theater",
-  "Command",
-];
-/*append all unitModB list items to Mod B HTML selector */
-for (let i = 0; i < unitModB.length; i++) {
-  let option = document.createElement("option");
-  option.text = unitModB[i];
-  option.value = i;
-  let select = document.getElementById("inputChpt2ModB");
-  select.appendChild(option);
-}
-
-/*unit org screen array for storing all img file names broken into two subsets for each of the two unit org selectors, note how a blank svg is added for the times when a user doesnt elect to use that selector*/
-const imgArrayName = [
-  [
-    "./img/blank.svg",
-    "./img/symbFrameActvInstFdlyK.svg",
-    "./img/symbFrameActvInstEnyK.svg",
-    "./img/symbFrameActvInstNeutK.svg",
-    "./img/symbFrameActvInstUnkK.svg",
-  ],
-  [
-    "./img/blank.svg",
-    "./img/symbAmpBTm.svg",
-    "./img/symbAmpBSqd.svg",
-    "./img/symbAmpBSect.svg",
-    "./img/symbAmpBPlt.svg",
-    "./img/symbAmpBCo.svg",
-    "./img/symbAmpBBn.svg",
-    "./img/symbAmpBGrpRgt.svg",
-    "./img/symbAmpBBde.svg",
-    "./img/symbAmpBDiv.svg",
-    "./img/symbAmpBCps.svg",
-    "./img/symbAmpBTa.svg",
-    "./img/symbAmpBAg.svg",
-    "./img/symbAmpBT.svg",
-    "./img/symbAmpBCmd.svg",
-  ],
-];
-
-/*unit org screen array for storing all the img files paired to the file name array, could probably create a larger data set to store these all consolidated, anyways... this is to align each specific image against the canvas appropriately so they all match up nicely */
-const imgArrayLocation = [
-  [
-    [0, 0, 0, 0, 0, 0, 0, 0], //proportionate placement complete ppc
-    [0, 0, 1600, 800, 0, 0, 300, 150], //friendly, ppc
-    [0, 0, 1600, 800, 0, 6, 300, 150], //enemy, ppc
-    [0, 0, 1600, 800, 0, 0, 300, 150], //neutral, ppc
-    [0, 0, 300, 300, 82.75, 11, 135, 135], //unknown, ppc
-  ],
-  [
-    [0, 0, 0, 0, 0, 0, 0, 0], //ppc
-    [0, 0, 50, 50, 129, 20, 32, 32], //team
-    [0, 0, 112, 108, 140.5, 20, 20, 18], //squad
-    [0, 0, 246, 108, 130, 20, 40, 18], //section
-    [0, 0, 380, 108, 120, 20, 60, 18], //platoon
-    [0, 0, 50, 50, 130.75, 15, 48, 48], //company
-    [0, 0, 50, 50, 126.5, 15, 48, 48], //battalion
-    [0, 0, 50, 50, 122.75, 15, 48, 48], //group or regiment
-    [0, 0, 50, 50, 126.75, 16, 48, 48], //brigade
-    [0, 0, 50, 50, 118, 16, 48, 48], //division
-    [0, 0, 85, 50, 104.5, 16, 92, 48], //corps
-    [0, 0, 100, 50, 100, 16, 98, 48], //theater army
-    [0, 0, 112, 50, 92.75, 16, 108, 48], //army group
-    [0, 0, 128, 50, 87.25, 16, 118, 48], //theater
-    [0, 0, 50, 50, 115, 16, 52, 52], //command
-  ],
-];
-
-/*unit org screen array for storing selector user inputs*/
-const imgSelectors = ["inputChpt2MainA", "inputChpt2ModB"];
 
 /*unit org screen image build function to run through list of input selections in imgTileArea array to pull from selector and push to img output in html wrapper2*/
-function funcUnitOrg() {
-  const canvas = document.getElementById("canvas1");
+function funcCreate() {
+  /*Instantiate Canvas & Clear Former Rect*/
+  const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.lineWidth = 1.2;
-  for (let i = 0; i < imgSelectors.length; i++) {
-    let imgInput = document.getElementById(imgSelectors[i]).value;
-    let imgVar = document.createElement("img");
-    let source = imgArrayName[i][imgInput];
-    console.log(source);
-    imgVar.src = source;
+  /*
+  BREAK
+  */
+  for (let i = 0; i < arrayCategory[screenSelVal].length - 1; i++) {
+    let selVal = document.getElementById(`selector${i}`).value;
+    let catVal = arrayCategory[screenSelVal][i + 1];
+    let imgInput = catVal[selVal][1];
+    let imgVar = new Image();
+    imgVar.src = imgInput;
     imgVar.onload = function () {
-      ctx.drawImage(
-        imgVar,
-        imgArrayLocation[i][imgInput][0],
-        imgArrayLocation[i][imgInput][1],
-        imgArrayLocation[i][imgInput][2],
-        imgArrayLocation[i][imgInput][3],
-        imgArrayLocation[i][imgInput][4],
-        imgArrayLocation[i][imgInput][5],
-        imgArrayLocation[i][imgInput][6],
-        imgArrayLocation[i][imgInput][7]
-      );
+      ctx.drawImage(imgVar, 0, 0, 3400, 1700, 0, 0, 300, 150);
     };
   }
 }
